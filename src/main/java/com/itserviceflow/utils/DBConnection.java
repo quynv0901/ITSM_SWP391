bf7c38cee15399e46debf6395a8841b2b345800f
Quy <Quynvhe180901@fpt.edu.vn>
2026-04-28 00:00:00 +0700

    fix db connect

--- src/main/java/com/itserviceflow/utils/DBConnection.java
+++ src/main/java/com/itserviceflow/utils/DBConnection.java
@@ -18,7 +18,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/itserviceflow_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "1234";
+    private static final String PASSWORD = "123456";
 
 
     static {
